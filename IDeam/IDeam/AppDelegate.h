8194e513f3bc9dfc7e31a02d4dae3f63f815ea8f
Bonway <bonway_huang@163.com>
2015-12-26 16:04:21 +0800

    IDeam的初始化

---
+++ IDeam/IDeam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IDeam
+//
+//  Created by Bonway(黄邦伟) on 15/12/26.
+//  Copyright © 2015年 Bonway(黄邦伟). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IDeam/IDeam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IDeam
+//
+//  Created by Bonway(黄邦伟) on 15/12/26.
+//  Copyright © 2015年 Bonway(黄邦伟). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
